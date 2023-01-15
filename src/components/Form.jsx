3d1b15286a7b4b3da22237a4bf26bd20df8f0b04
Collins Chirchir <collinskchirchir@gmail.com>
2023-01-15 11:11:42 +0300

    Added Form component

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -5,19 +5,23 @@ const Form = () => {
   const [qty, setQty] = useState(0)
   const [price, setPrice] = useState(0)
   
+  const handleSubmit = (e)=>{
+    e.preventDefault();
+    setFormData([...formData, {date, item, qty, price}])
+  }
   return(
-    <form className="row p-3">
+    <form className="row p-3" onSubmit={handleSubmit}>
       <div className="col-3">
-        <input name="date" className="form-control form-control-sm" type="date" placeholder="date"/>
+        <input name="date" onChange={(e)=>{setDate    }} className="form-control form-control-sm" value={date} type="date" placeholder="date"/>
       </div>
       <div className="col-3">
-        <input name="item" className="form-control form-control-sm" type="text" placeholder="item"/>
+        <input name="item" className="form-control form-control-sm" type="text" value={item} placeholder="item"/>
       </div>
       <div className="col-3">
-        <input name="qty" className="form-control form-control-sm" type="number" placeholder="qty"/>
+        <input name="qty" className="form-control form-control-sm" type="number" value={qty} placeholder="qty"/>
       </div>
       <div className="col-3">
-        <input name="price" className="form-control form-control-sm" type="number" placeholder="price"/>
+        <input name="price" className="form-control form-control-sm" type="number" value={price} placeholder="price"/>
       </div>
       <div className="col-3">
         <button  className="btn btn-sm btn-success mx-auto">Submit</button>
